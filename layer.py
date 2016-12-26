c2d0ae363d8373d9775afb58f3816476f3554ede
Abhinav Kumar <abhinavkumar.sieger@gmail.com>
2016-12-26 17:40:24 +0530

    Backup

--- layer.py
+++ layer.py
@@ -22,15 +22,29 @@ class layer():
                     
                 i=i+1
     
-    def initialize_layer(self):
-        for i in range(self.num_of_units):
-            shape=self.i.shape
-            for j in range(shape[0]):
-                for k in range(shape[1]):
-                    self.i[j][k]=node(self.layer_type,self.bias_property[i])
-                    
+                       
+    def create_connection(self,foreward_layer,connection_list)
+    ''' Argument 1: Give the foreward layer object to which we want to connection_list
+        Argument 2: its a list of tuple of connection from each subunits in this layer to all the subunits in foreward layer
+                        Possible connection now are ('one_one','one_to_all','none')
+                            'one_one' : creates one to one mapping from current subunit to the specified subunit in next layer
+                            'one_to_all : creates mapping where this all the nodes of this subunit are mapped to all the nodes of next layer subunit.
+                            Later 'convolution' will be added. '''
+        
+        # Error Checking in argument
+        if len(connection_list) != self.num_of_units:
+            print ('Error.Give correct number of connection for each subunits of this layer')
+        # Starting to connect
+        self.connection=()   
+        for unit_connection_tup in connection_list:
+            # Error check for given tuple
+            if len(unit_connection_tup) != foreward_layer.num_of_units :
+                print ('Error.Give correct number of connection for each subunits of next layer')
+            self.connection=self.connection+unit_connection_tup
+            for i in range(self.num_of_units):
+                shape=self.i.shape
+                for j in range(shape[0]):
+                    for k in range(shape[1]):
+                        self.i[j][k]=node(self.layer_type,self.bias_property[i])
+                        
             
-    
-    #def find_connection(self,foreward_layer)
-
-
