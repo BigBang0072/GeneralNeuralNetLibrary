5d441493b7741f2db20150a681325f558f6f082e
Abhinav Kumar <abhinavkumar.sieger@gmail.com>
2016-12-31 19:49:13 +0530

    Numerical Gradient to verify model consistency
    Still Debugging!!

--- layer.py
+++ layer.py
@@ -9,6 +9,8 @@ class layer():
             
         self.num_of_units=len(unit_property_list)
         self.layer_type=layer_type
+        if layer_type=='output':
+            self.cost_incurred=0
         self.bias_property=()
         self.sub_units=()
         self.connections=()
@@ -34,9 +36,7 @@ class layer():
                 for k in range(shape[1]):
                     self.sub_units[i][j][k]=node(self.layer_type,self.bias_property[i])
                     #print "hi"
-                    
-            
-    
+                      
     def create_connection(self,foreward_layer,connection_list):
         ''' Argument 1: Give the foreward layer object to which we want to connection_list
             Argument 2: its a list of tuple of connection from each subunits in this layer to all the subunits in foreward layer
@@ -73,6 +73,9 @@ class layer():
                     theta_temp=np.random.rand(shape[0],shape[1])
                     gradient_temp=np.random.rand(shape[0],shape[1])
                     #print theta_temp
+                elif unit_connection=='none':
+                    theta_temp=np.random.rand(0,0)
+                    gradient_temp=np.random.rand(0,0)
                 unit_shape=self.sub_units[i].shape
                 #print unit_shape
                 for k in range(unit_shape[0]):
@@ -83,8 +86,7 @@ class layer():
                         self.sub_units[i][k][l].Gradient=self.sub_units[i][k][l].Gradient+(gradient_temp,)
                         self.sub_units[i][k][l].connection_type=self.sub_units[i][k][l].connection_type+(unit_connection,)
                 
-            
-            
+                      
     # Foreward_propagation of layer to next layer
     def layer_foreward_propagate(self,foreward_layer):
         for i,sub_unit_current in enumerate(self.sub_units):
@@ -106,13 +108,16 @@ class layer():
                                     
     
     # ONLY FOR HIDDEN layer and OUTPUT layer. Rectifying the z_val to convert to a_val.
-    def inlayer_foreward_propagation(self):
+    def inlayer_foreward_propagate(self):
         if self.layer_type=='hidden' or self.layer_type=='output':
             for i,sub_unit in enumerate(self.sub_units):
                 shape=sub_unit.shape
-                for j in range(shape[0]):
-                    for k in range(shape[1]):
-                        sub_unit[j][k].a_val=self.rectification_function_tup[0](sub_unit[j][k].z_val)
+                if self.bias_property[i]=='un_biased':
+                    for j in range(shape[0]):
+                        for k in range(shape[1]):
+                            sub_unit[j][k].a_val=self.rectification_function_tup[0](sub_unit[j][k].z_val)
+                else:
+                    print ""
         else:
             print ("inlayer activation not valid for input layer")
             
@@ -146,7 +151,7 @@ class layer():
                             for l in range(shape_unit_current[1]):
                                 for m in range(shape_unit_foreward[0]):
                                     for n in range(shape_unit_foreward[1]):
-                                        self.sub_units[i][k][l].Gradient[j][m][n]=foreward_layer.sub_unit[j][m][n].error_delta*self.sub_units[i][k][l].a_val
+                                        self.sub_units[i][k][l].Gradient[j][m][n]=foreward_layer.sub_units[j][m][n].error_delta*self.sub_units[i][k][l].a_val
                                         self.sub_units[i][k][l].error_delta=self.sub_units[i][k][l].error_delta+foreward_layer.sub_units[j][m][n].error_delta*self.sub_units[i][k][l].Theta[j].item(m,n)
                         
         elif self.layer_type=='input':
@@ -166,17 +171,87 @@ class layer():
                             for l in range(shape_unit_current[1]):
                                 for m in range(shape_unit_foreward[0]):
                                     for n in range(shape_unit_foreward[1]):
-                                        self.sub_units[i][k][l].Gradient[j][m][n]=foreward_layer.sub_unit[j][m][n].error_delta*self.sub_units[i][k][l].a_val
+                                        self.sub_units[i][k][l].Gradient[j][m][n]=foreward_layer.sub_units[j][m][n].error_delta*self.sub_units[i][k][l].a_val
                                         #self.sub_units[i][k][l].error_delta=self.sub_units[i][k][l].error_delta+foreward_layer.sub_units[j][m][n].error_delta*self.sub_units[i][k][l].Theta[j].item(m,n)
-    
-        
+          
     def inlayer_back_propagate(self):
         ''' To get the error delta with sigmoid gradient applied which was left in layer_back_propagate'''
-        for i,sub_unit in enumerate(self.sub_units):
-            shape=sub_unit.shape
-            for j in range(shape[0]):
-                for k in range(shape[1]):
-                    sub_unit[j][k].error_delta=self.rectification_gradient_tup[0](sub_unit[j][k].error_delta)
+        if self.layer_type =='hidden':
+            for i,sub_unit in enumerate(self.sub_units):
+                shape=sub_unit.shape
+                for j in range(shape[0]): 
+                    for k in range(shape[1]):
+                        sub_unit[j][k].error_delta=self.rectification_gradient_tup[0](sub_unit[j][k].error_delta)
                     
+    def cost_calculation(self):
+        '''Only valid for output layer'''
+        if self.layer_type=='output':
+            for i,sub_unit in enumerate(self.sub_units):
+                shape=sub_unit.shape
+                for j in range(shape[0]):
+                    for k in range(shape[1]):
+                        self.cost_incurred=self.cost_incurred-((1-sub_unit[j][k].Y)*math.log(1-sub_unit[j][k].a_val)+(sub_unit[j][k].Y)*math.log(sub_unit[j][k].a_val))
+        else:
+            print "This function is only defined for the output layer. Tin ton tin"
+     
+
+     
+def gradient_checking(all_layer_list):
+    ''' To check if the model is consistent by calculating gradient numerically and then comparing it to gradient calculated trough back propagation'''
+    epsilon=math.pow(10,-4)     #as directed in exercise by Ng sahab.
+    for i,layer in enumerate(all_layer_list):
+        for j,sub_unit in enumerate(layer.sub_units):
+            unit_shape=sub_unit.shape
+            for k in range(unit_shape[0]):
+                for l in range(unit_shape[1]):
+                    for m,theta in enumerate(sub_unit[k][l].Theta):
+                        theta_shape=theta.shape
+                        for n in range(theta_shape[0]):
+                            for o in range(theta_shape[1]):
+                                #print all_layer_list
+                                temp_theta_holder= theta[n][o]
+                                theta[n][o]=temp_theta_holder+epsilon
+                                cost_epsilon_plus=give_cost(all_layer_list)
+                                #print all_layer_list
+                                all_layer_list.reverse()
+                                theta[n][o]=temp_theta_holder-epsilon
+                                cost_epsilon_minus=give_cost(all_layer_list)
+                                all_layer_list.reverse()
+                                theta[n][o]=temp_theta_holder
+                                cost=give_cost(all_layer_list)
+                                all_layer_list.reverse()
+                                numerical_gradient=(cost_epsilon_plus-cost_epsilon_minus)/(2*epsilon)
+                                actual_gradient=sub_unit[k][l].Gradient[m][n][o]
+                                if math.fabs(numerical_gradient-actual_gradient)<0.0009:
+                                    flag='PASSED'
+                                else:
+                                    flag='FAILED'
+                                
+                                print ("Actual:",actual_gradient," Numerical:",numerical_gradient," Status:",flag)
+                                
+def give_cost(all_layer_list):
+    for i,layer in enumerate(all_layer_list):
+        if layer.layer_type=='input':
+            layer.layer_foreward_propagate(all_layer_list[i+1])
+        elif layer.layer_type=='hidden':
+            layer.inlayer_foreward_propagate()
+            layer.layer_foreward_propagate(all_layer_list[i+1])
+        else:
+            layer.inlayer_foreward_propagate()
+            
+    
+    
+    all_layer_list.reverse()
+    all_layer_list[0].cost_calculation()
+    for i,layer in enumerate(all_layer_list):
+        if layer.layer_type=='input':
+            layer.layer_back_propagate(all_layer_list[i-1])
+        elif layer.layer_type=='hidden':
+            layer.layer_back_propagate(all_layer_list[i-1])
+            layer.inlayer_back_propagate()
+        else:
+            layer.layer_back_propagate('none')
+    
+    return all_layer_list[0].cost_incurred
     
             
